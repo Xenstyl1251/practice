00cc49ed7ff5c22fa6a7d80510366d9bf06005fc
Xenstyl1251 <congtv1251@gmail.com>
2023-06-26 23:08:17 -0700

    i change the text

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>WELCOME!</h1>
+        <h1>WELCOME! 1</h1>
         <h2>WELCOME!</h2>
         <span>WELCOME!</span>
         <label>WELCOME!</label>
